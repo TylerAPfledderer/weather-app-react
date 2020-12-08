82187f34e5880ec566a5753abe1121773a6752b4
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2020-12-08 11:25:09 -0500

    "Include initial Context API, Open Weather API Call, and template structure for single weather day. Add weather icons font and stylesheets. Add node-sass."

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useContext } from 'react';
+import { WeatherAppContext } from './components/Context';
+import './scss/App.scss';
 
 function App() {
+
+  const { city, forecast } = useContext(WeatherAppContext);
+ 
+  const cityTemp = {name: "Raleigh"}
+  const forecastTemplate = {
+    "main": {
+      "temp": 36.16
+    },
+    "weather": [
+      {
+        "description": "clear sky",
+        "icon": "04n"
+      }
+    ],
+    "dt_text": "2020-12-08 15:00:00"
+  }
+
+  const currentDate = new Date(forecastTemplate.dt_text);
+  const currentDay = new Intl.DateTimeFormat('en-US', {weekday: "long"}).format(currentDate);
+
+  const currentMonthDay = new Intl.DateTimeFormat('en-US', {month: "long", day: "numeric"}).format(currentDate);
+  const currentWeather = `http://openweathermap.org/img/wn/${forecastTemplate.weather[0].icon}@2x.png`
+  const mainTemp = Math.ceil(forecastTemplate.main.temp);
+  const weatherDescription = 
+    forecastTemplate.weather[0].description.charAt(0).toUpperCase() 
+    + 
+    forecastTemplate.weather[0].description.slice(1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>5-Day Weather Forecast</h1>
+        <span>{cityTemp.name}</span>
       </header>
-    </div>
+      <main>
+        <section>
+          <div>
+            <h3>{currentDay}</h3>
+            <span>{currentMonthDay}</span>
+            <div>
+              <i className="wi wi-day-sunny" />
+              <span>{mainTemp}<img src={currentWeather} alt={weatherDescription}/></span>
+            </div>
+            <span>{weatherDescription}</span>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Context/index.js
@@ -0,0 +1,36 @@
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const WeatherAppContext = createContext();
+
+export const Provider = ({children}) => {
+
+    const [data, setData] = useState([]);
+
+    const getWeather = async () => {
+        try {
+            const weatherData = await axios.get("http://api.openweathermap.org/data/2.5/forecast?q=Raleigh,us&units=imperial&appid=79709e9bb8ffe2a8b3e3ef6b8f3be053");
+            setData(weatherData.data);           
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    console.log(data.list);
+
+    useEffect(() => {
+        // getWeather();
+    }, []);
+
+
+    return (
+        <WeatherAppContext.Provider
+            value={{
+                city: data.city,
+                forecast: data.list
+            }}
+        >
+            {children}
+        </WeatherAppContext.Provider>
+    );
+}
