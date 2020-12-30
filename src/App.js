82d9aff9acfd6d6897a231594b73e0525e40bd6a
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2020-12-30 18:41:48 -0500

    "Remove temporary data and refactor minMaxTempsFromData() in App.js. Comment out blocks of statements in App.js while attending to bug in data fetch call."

--- src/App.js
+++ src/App.js
@@ -3,82 +3,32 @@ import { WeatherAppContext } from "./components/Context";
 import "./scss/App.scss";
 
 function App() {
-  const { forecast } = useContext(WeatherAppContext);
-  console.log("🚀 ~ file: App.js ~ line 7 ~ App ~ forecast", forecast);
+  const { forecast, city } = useContext(WeatherAppContext);
+  // const { main } = forecast;
 
-  const cityTemp = { name: "Raleigh" }; // Replace this in the return with city from useContext();
-  const forecastTemplate = {
-    // Replace this in the return with forecast from useContext();
-    main: {
-      temp: 36.16,
-    },
-    weather: [
-      {
-        description: "clear sky",
-        icon: "04n",
-      },
-    ],
-    dt_text: 1608778800,
-  };
+  // const currentDate = new Date(forecast[0].dt_text);
+  // const currentDay = new Intl.DateTimeFormat("en-US", {
+  //   weekday: "long",
+  // }).format(currentDate);
 
-  const {
-    main: { temp },
-  } = forecastTemplate;
-
-  const currentDate = new Date(forecastTemplate.dt_text);
-  const currentDay = new Intl.DateTimeFormat("en-US", {
-    weekday: "long",
-  }).format(currentDate);
-
-  const currentMonthDay = new Intl.DateTimeFormat("en-US", {
-    month: "long",
-    day: "numeric",
-  }).format(currentDate);
-  const currentWeather = `http://openweathermap.org/img/wn/${forecastTemplate.weather[0].icon}@2x.png`;
-  const mainTemp = Math.ceil(temp);
-  const weatherDescription =
-    forecastTemplate.weather[0].description.charAt(0).toUpperCase() +
-    forecastTemplate.weather[0].description.slice(1);
-
-  // const filterObjArrByDateProp = (arr, prop) => {
-  //   let prevObj;
-  //   const uniqueDates = [];
-
-  //   // Ensure an array is returned by Provider fetch call before iterating
-  //   if (arr) {
-  //     arr.forEach(object => {
-  //       if (prevObj === undefined) {
-  //         // Set prevObj then move to next object to do clean check
-  //         prevObj = object;
-  //         uniqueDates.push(object); // This object also needs to be stored as a unique
-  //         return;
-  //       }
-
-  //       // Define day by number for previous and current object from the "dt" prop
-  //       const prevDate = new Date(prevObj[prop]).getDay();
-  //       const currDate = new Date(object[prop]).getDay();
-
-  //       if (prevDate === currDate) {
-  //         // Immediately return as we need only one object per day value given.
-  //         return;
-  //       }
-
-  //       // Store object with unique day
-  //       uniqueDates.push(object);
-  //       prevObj = object;
-  //     });
-  //   }
-
-  //   return uniqueDates;
-  // }
-
-  // console.log(filterObjArrByDateProp(forecast, "dt_txt"));
+  // const currentMonthDay = new Intl.DateTimeFormat("en-US", {
+  //   month: "long",
+  //   day: "numeric",
+  // }).format(currentDate);
+  // const currentWeather = `http://openweathermap.org/img/wn/${forecast[0].weather.icon}@2x.png`;
+  // const mainTemp = Math.ceil(forecast.main.temp);
+  // const weatherDescription =
+  //   forecast[0].weather.description.charAt(0).toUpperCase() +
+  //   forecast[0].weather.description.slice(1);
 
   const minMaxTempsFromData = function() {
-    let prevObj;
-    let newObjArr = [];
-    let tempArr = [];
 
+    /**
+     * Function to return an object of the data for a single day and it's min/max temps
+     * @param {String} date 
+     * @param {Number} minTemp 
+     * @param {Number} maxTemp 
+     */
     const newObject = (date, minTemp, maxTemp) => {
       return {
         date,
@@ -87,12 +37,16 @@ function App() {
       };
     };
 
+    // Check if state contains the data on render before running the loop
     if (forecast) {
-
+      let prevObj;
+      let newObjArr = [];
+      let tempArr = [];
+      // Looping through the array of forecast data
       forecast.forEach((object, index) => {
         if (prevObj === undefined) {
-          prevObj = object;
           tempArr.push(object.main.temp);
+          prevObj = object;
           return; //Because we only want to log initial values for the following checks
         }
 
@@ -102,36 +56,33 @@ function App() {
         if (currDate === prevDate) {
           tempArr.push(object.main.temp);
         } else {
-          newObjArr.push(newObject(prevObj.dt_txt, Math.min(...tempArr), Math.max(...tempArr)));
-          prevObj = object;
+          newObjArr.push( newObject(prevObj.dt_txt, Math.min(...tempArr), Math.max(...tempArr)) );
           tempArr = [object.main.temp];
+          prevObj = object;
         }
 
         if (index === forecast.length - 1) {
-          newObjArr.push(newObject(prevObj.dt_txt, Math.min(...tempArr), Math.max(...tempArr)));
+          newObjArr.push( newObject(prevObj.dt_txt, Math.min(...tempArr), Math.max(...tempArr)) );
         }
       });
-    }
 
-    return newObjArr;
+      return newObjArr;
+    }
   };
-  console.log(
-    "🚀 ~ file: App.js ~ line 111 ~ minMaxTempsFromData ~ minMaxTempsFromData",
-    minMaxTempsFromData()
-  );
+  console.log("🚀 ~ file: App.js ~ line 81 ~ minMaxTempsFromData ~ minMaxTempsFromData", minMaxTempsFromData());
 
   return (
     <>
       <header>
         <h1>5-Day Weather Forecast</h1>
-        <span>{cityTemp.name}</span>
+        {/* <span>{city.name}</span> */}
       </header>
       <main>
         <section>
           {/* <ForecastList /> */}
           <div>
             {/* <ForecastCard /> */}
-            <h3>{currentDay}</h3>
+            {/* <h3>{currentDay}</h3>
             <span>{currentMonthDay}</span>
             <div>
               <i className="wi wi-day-sunny" />
@@ -140,7 +91,7 @@ function App() {
                 <img src={currentWeather} alt={weatherDescription} />
               </span>
             </div>
-            <span>{weatherDescription}</span>
+            <span>{weatherDescription}</span> */}
           </div>
         </section>
       </main>
--- src/components/Context/index.js
+++ src/components/Context/index.js
@@ -10,12 +10,12 @@ export const Provider = ({children}) => {
     const getWeather = async () => {
         try {
             const weatherData = await axios.get("http://api.openweathermap.org/data/2.5/forecast?q=Raleigh,us&units=imperial&appid=79709e9bb8ffe2a8b3e3ef6b8f3be053");
-            setData(weatherData.data);           
+            setData(weatherData.data);        
         } catch (err) {
             console.error(err.message);
         }
     }
-
+    
     useEffect(() => {
         getWeather();
     }, []);
@@ -32,17 +32,3 @@ export const Provider = ({children}) => {
         </WeatherAppContext.Provider>
     );
 }
-
-/**
-    const filterDuplicateObjVals = (arr, prop) => {
-        const newArr = [];
-        let prevItem;
-        array.forEach(currentItem => {
-            if (prevItem === undefined || prevItem[prop] !== currentItem[prop]) {
-            newArr.push(currentItem);
-            }
-            prevItem = currentItem;
-        });
-        return newArr;
-    } 
- */
