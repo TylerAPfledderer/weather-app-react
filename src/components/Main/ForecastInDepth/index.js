188b86a8aff7327777e8b2bf511dbca4bf110611
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-02-06 16:18:48 -0500

    Create folder for ForecastInDepth component. Generate ForecastInDepth scss module and provide styling for the ul element and center the wrapper. Create useHourData dependent hook in ForecastInDepth component and render map of list items for each piece of hourly data.

---
+++ src/components/Main/ForecastInDepth/index.js
@@ -0,0 +1,46 @@
+import { useContext } from 'react';
+import { WeatherAppContext } from '../../Context';
+import InDepthLink from '../../InDepthLink';
+
+import { container, wrapper, hourlyList } from './ForecastInDepth.module.scss';
+
+const useHourData = (thisDate) => {
+  const { hourForecast } = useContext(WeatherAppContext);
+  const dayOfMonth = thisDate && thisDate.getDate();
+  const currentHours =
+    thisDate &&
+    hourForecast.filter(
+      (item) => new Date(item.dt * 1000).getDate() === dayOfMonth
+    );
+  console.log(currentHours && currentHours.slice(0, 8));
+  return currentHours && currentHours.slice(0, 8);
+};
+
+const ForecastInDepth = ({ location }) => {
+  const eightHours = useHourData(location.thisDate);
+  return (
+    <div className={container}>
+      <div className={wrapper}>
+        <h1>Today</h1>
+        <h2>January 18, 2021</h2>
+        <ul className={hourlyList}>
+          {eightHours &&
+            eightHours.map(({ dt, pop, weather, temp }, index) => (
+              <li key={index}>
+                <span>{dt}</span>
+                <i className={`wi wi-owm-${weather[0].id}`}></i>
+                <span>
+                  {Math.floor(temp)} <i className='wi wi-degrees'></i>
+                </span>
+                <span>{`Precip: ${pop * 100}%`}</span>
+              </li>
+            ))}
+        </ul>
+      </div>
+
+      <InDepthLink path='/'>Current Forecast</InDepthLink>
+    </div>
+  );
+};
+
+export default ForecastInDepth;
