16e1ea2e8cd0ca37e3063f36e67cb6ecb111779c
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-02-06 14:13:08 -0500

    Create getDateInfo function in the NavList component to create the object of date formats to render when mapping the ForecastDayLink components.

--- src/components/NavBar/NavList.js
+++ src/components/NavBar/NavList.js
@@ -2,6 +2,37 @@ import { useContext } from 'react';
 import { WeatherAppContext } from '../Context';
 import ForecastDayLink from './ForecastDayLink';
 
+/**
+ * Function to return an object of date formats
+ * @param {Object} date - the Date object passed in
+ * @return {Object} The date formats to desconstruct on invoke
+ */
+const getDateInfo = (date) => {
+  if (!(date instanceof Date)) {
+    const err = new Error(
+      'The provided argument for the getDateInfo function is not a valid Date object.'
+    );
+
+    return console.error(err.message);
+  }
+
+  let weekday = new Intl.DateTimeFormat('en-US', {
+    weekday: 'long',
+  }).format(date);
+
+  if (date.getDate() === new Date().getDate()) {
+    weekday = 'Today';
+  } else if (date.getDate() === new Date().getDate() + 1) {
+    weekday = 'Tomorrow';
+  }
+  const monthDay = new Intl.DateTimeFormat('en-US', {
+    month: 'numeric',
+    day: 'numeric',
+  }).format(date);
+
+  return { date, weekday, monthDay };
+};
+
 const NavList = () => {
   const { dailyForecast } = useContext(WeatherAppContext);
 
@@ -11,14 +42,9 @@ const NavList = () => {
         dailyForecast
           .slice(0, 5)
           .map(({ dt, temp: { min, max }, weather: [{ id }] }, index) => {
-            const date = dt !== 0 && new Date(dt * 1000);
-            const weekday = new Intl.DateTimeFormat('en-US', {
-              weekday: 'short',
-            }).format(date);
-            const monthDay = new Intl.DateTimeFormat('en-US', {
-              month: 'numeric',
-              day: 'numeric',
-            }).format(date);
+            const { date, weekday, monthDay } = getDateInfo(
+              dt !== 0 && new Date(dt * 1000)
+            );
             return (
               <ForecastDayLink
                 key={index}
