75330e065ff3d4c0cf4a48ee391fb63e0fd1d5a8
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-02-14 15:12:58 -0500

    Use margin shorthand to include margin top value, use rgba value for background and add box shadow in InDepthLink component.

--- src/components/InDepthLink.js
+++ src/components/InDepthLink.js
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
 
 const btnStyle = {
-  background: '#aaa',
+  background: 'rgba(0,0,0,0.5)',
+  boxShadow: '0 5px 10px 0 rgba(255,255,255,0.3)',
   borderRadius: '31px',
   color: '#fff',
   fontWeight: 'bold',
   textDecoration: 'none',
   padding: `calc((44px - 1rem) / 2) 16px`,
-  marginBottom: '16px',
-  marginRight: '16px',
+  margin: '16px 16px 16px 0',
   alignSelf: 'flex-end'
 };
 
