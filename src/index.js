d42defb3297fe1db57d01d0a0ab942671bf52df0
Tyler Pfledderer <tyler.pfledderer@gmail.com>
2021-02-14 14:52:50 -0500

    Add basename prop value "/weather-app-react" to Router in index

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
     <Provider>
-      <Router>
+      <Router basename='/weather-app-react'>
         <App />
       </Router>
     </Provider>
